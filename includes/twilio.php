032f2c9465b10f35b71e4f92633fc65faeb6af4a
Chris Chasm <chris@chasm.solutions>
2022-01-06 13:58:04 -0700

    Bump to 1.2

--- includes/twilio.php
+++ includes/twilio.php
@@ -1,8 +1,6 @@
 <?php
 if ( !defined( 'ABSPATH' ) ) { exit; } // Exit if accessed directly
 
-// @todo add custom email sending service
-
 add_filter( 'dt_message_methods', function( $list ){
     $list['twilio'] = [
         'key' => 'twilio',
