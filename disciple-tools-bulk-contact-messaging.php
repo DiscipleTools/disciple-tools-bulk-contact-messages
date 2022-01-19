76325619411ab3e735bde4546d2c4eb4a155f61f
Chris Chasm <chris@chasm.solutions>
2022-01-19 05:24:56 -0700

    Bump 1.5

--- disciple-tools-bulk-contact-messaging.php
+++ disciple-tools-bulk-contact-messaging.php
@@ -5,7 +5,7 @@
  * Description: Disciple.Tools - Bulk Contact Messages adds a feature to the contact lists page to send bulk messages to selected list of contacts.
  * Text Domain: disciple-tools-bulk-contact-messaging
  * Domain Path: /languages
- * Version:  1.4
+ * Version:  1.5
  * Author URI: https://github.com/DiscipleTools
  * GitHub Plugin URI: https://github.com/DiscipleTools/disciple-tools-bulk-contact-messaging
  * Requires at least: 4.7.0
